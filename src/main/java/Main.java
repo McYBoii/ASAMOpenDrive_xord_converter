21c16099ca0d83e1c6935ba5a823ff3e3eb8c7bb
Donert Rinaldó <donert.rinaldo@gmail.com>
2024-05-04 17:09:35 +0200

    Iteration on roads seems to work fine, can sex successor and predecessor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,9 @@ import java.io.NotActiveException;
 
 public class Main {
 
+    /**Use this function to write the header of the problem file.
+     * @return file header
+     */
     public static String problemFileHeader(){
         return """
 %Metamodel
@@ -67,8 +70,46 @@ Map(map).
 """;
     }
 
-    public static String elementsSetter(String s){
-        return "elements(map, "+s+").\n";
+    /** Use this function to generate new instances of Road
+     * @param id ID of the Road
+     * @return Refinery creation text
+     */
+    public static String createRoad(String id){
+        return "Road("+id+").\n";
+    }
+
+    /** Use this function to generate new instances of Junction
+     * @param id ID of the Junction
+     * @return Refinery creation text
+     */
+    public static String createJunction(String id){
+        return "Junction("+id+").\n";
+    }
+
+    /** Use this function to set the elements attribute of the Map.
+     * @param id ID of the element
+     * @return Refinery attribute setter text
+     */
+    public static String elementsSetter(String id){
+        return "elements(map, "+id+").\n";
+    }
+
+    /** Use this function to set the successor of a Road
+     * @param roadID the road we set the successor for
+     * @param successorID the successor
+     * @return Refinery attribute setter text
+     */
+    public static String successorSetter(String roadID, String successorID){
+        return "successor("+roadID+", "+successorID+").\n";
+    }
+
+    /** Use this function to set the predecessor of a Road
+     * @param roadID the road we set the predecessor for
+     * @param predecessorID the predecessor
+     * @return Refinery attribute setter text
+     */
+    public static String predecessorSetter(String roadID, String predecessorID){
+        return "predecessor("+roadID+", "+predecessorID+").\n";
     }
 
     static String FILEPATH = "model.problem";
@@ -88,33 +129,68 @@ Map(map).
 
             Element rootElement = document.getDocumentElement();
 
+            // 3 main map elements from the file
             NodeList roads = rootElement.getElementsByTagName("road");
             NodeList junctions = rootElement.getElementsByTagName("junction");
             NodeList junctiongroups = rootElement.getElementsByTagName("junctionGroup");
 
-            int LaneLinkCounter = 0; // LaneLink is missing ID so we generate for it.
+            // LaneLink is missing ID so we generate for it.
+            int LaneLinkCounter = 0;
 
+            // Making the file or overrideing it
             File output = new File(FILEPATH);
 
             FileWriter fileWriter = new FileWriter(output);
 
+            // Writing the metamodel and Map instance see above what we write here.
             fileWriter.write(problemFileHeader());
 
+            // Iterating every road and witing to the file as we progress.
             for (int i = 0; i < roads.getLength(); i++) {
                    Node road = roads.item(i);
 
                    if(road.getNodeType() == Node.ELEMENT_NODE){
                        Element eroad = (Element) road;
 
-                       fileWriter.write(elementsSetter("Road"+eroad.getAttribute("id")));
+                       // Generating an ID to prefix to child elements.
+                       String currentRoadID = "road"+eroad.getAttribute("id");
 
+                       //Creating the road and adding it to the elements of the map.
+                       fileWriter.write(createRoad(currentRoadID));
+
+                       fileWriter.write(elementsSetter(currentRoadID));
+
+                       //Getting the links from the current road, but we only use the first one.
                        NodeList links = eroad.getElementsByTagName("link");
+                       Element link = (Element)links.item(0);
 
+                       NodeList successors = link.getElementsByTagName("successor");
+                       NodeList predecessors = link.getElementsByTagName("predecessor");
+
+                       //Sometimes successor and predecessor are not present we take care of this here.
+                       if(successors.getLength() == 1){
+                           Element successor = (Element)successors.item(0);
+                           String successorID = successor.getAttribute("elementType")+successor.getAttribute("elementId");
+                           fileWriter.write(successorSetter(currentRoadID, successorID));
+                       }
+                       if(predecessors.getLength() == 1){
+                           Element predecessor = (Element)predecessors.item(0);
+                           String predecessorID = predecessor.getAttribute("elementType")+predecessor.getAttribute("elementId");
+                           fileWriter.write(predecessorSetter(currentRoadID, predecessorID));
+                       }
+
+                       //Getting the laneSections and processing them
                        NodeList lanesections = eroad.getElementsByTagName("laneSection");
+                       for (int j = 0; j < lanesections.getLength(); j++) {
+                           Element lanesection = (Element) lanesections.item(j);
 
-                       System.out.println(lanesections.getLength());
+                           NodeList leftlanes = lanesection.getElementsByTagName("left");
 
-                       System.out.println(links.getLength());
+
+                           NodeList centerlanes = lanesection.getElementsByTagName("center");
+                           NodeList rightlanes = lanesection.getElementsByTagName("right");
+
+                       }
                    }
             }
             
